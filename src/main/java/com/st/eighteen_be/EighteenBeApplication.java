4ad41d76dc8c1ca79b0cea98c521bb0d9bd0331d
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-03-18 00:38:21 +0900

    build(master): 첫 커밋

---
+++ src/main/java/com/st/eighteen_be/EighteenBeApplication.java
@@ -0,0 +1,13 @@
+package com.st.eighteen_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EighteenBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EighteenBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/st/eighteen_be/EighteenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st.eighteen_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EighteenBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
