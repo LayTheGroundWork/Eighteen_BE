b4ca27b14108b92c4e636b6daf71fe771a780a9f
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-04-03 16:56:33 +0900

    feat: 몽고 DB 추가중

---
+++ src/main/java/com/st/eighteen_be/chat/constant/MongoConst.java
@@ -0,0 +1,23 @@
+package com.st.eighteen_be.chat.constant;
+
+/**
+ * packageName    : com.st.eighteen_be.chat.constant
+ * fileName       : MongoConst
+ * author         : ipeac
+ * date           : 2024-04-03
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024-04-03        ipeac       최초 생성
+ */
+public class MongoConst {
+    public static final String MONGO_DB_NAME = "eighteen-be";
+    public static final String MONGO_COLLECTION_NAME = "chatroom";
+    
+    public static final String MONGO_HOST = "localhost";
+    
+    public static final int MONGO_PORT = 27017;
+    
+    public static final String MONGO_CONTAINER_IMAGE_NAME = "mongo:7";
+}
