dd9903383a43f2d911f8f608d4b80fb9263b357a
ehgur <ehgur062300@gmail.com>
2024-04-11 16:14:17 +0900

    feat: 생성일 및 수정일 관련 추상 클래스 작성

---
+++ src/main/java/com/st/eighteen_be/domain/BaseTimeEntity.java
@@ -0,0 +1,35 @@
+package com.st.eighteen_be.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        String customLocalDateTimeFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        LocalDateTime parsedCreateDate = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.createdAt = parsedCreateDate;
+        this.modifiedAt = parsedCreateDate;
+    }
+}
