4d86bb2b2dda2d31700ab8b1df9f09cf966d7245
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-04-16 15:30:03 +0900

    fix: 몽고DB 와 JPA 레포지터리 빈 중복탐색문제 발생으로 해결

---
+++ src/main/java/com/st/eighteen_be/config/repository/MongoConfig.java
@@ -0,0 +1,20 @@
+package com.st.eighteen_be.config.repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+/**
+ * packageName    : com.st.eighteen_be.config.repository
+ * fileName       : MongoConfig
+ * author         : ipeac
+ * date           : 24. 4. 16.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 4. 16.        ipeac       최초 생성
+ */
+@Configuration
+@EnableMongoRepositories(basePackages = "com.st.eighteen_be.chat.repository.mongo")
+public class MongoConfig {
+}
