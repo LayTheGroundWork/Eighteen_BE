055e074bf6e5e9d834ae2e967b46d30765a8c41e
ehgur <ehgur062300@gmail.com>
2024-04-19 19:53:01 +0900

    refactor: 파일 위치 변경 및 수정

--- src/main/java/com/st/eighteen_be/common/basetime/BaseEntity.java
+++ src/main/java/com/st/eighteen_be/common/basetime/BaseEntity.java
@@ -1,11 +1,13 @@
 package com.st.eighteen_be.common.basetime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -13,10 +15,13 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public class BaseEntity {
-    
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @CreatedDate
-    private LocalDateTime createdDate;
-    
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
 }
