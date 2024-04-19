76a3c323211f0bc83b37229e8836765da00b17c8
ehgur <ehgur062300@gmail.com>
2024-04-19 19:53:01 +0900

    refactor: 파일 위치 변경 및 수정

--- src/main/java/com/st/eighteen_be/common/basetime/BaseEntity.java
+++ src/main/java/com/st/eighteen_be/common/basetime/BaseEntity.java
@@ -10,6 +10,7 @@ import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -20,10 +21,13 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 @AllArgsConstructor
 public abstract class BaseEntity {
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @CreatedDate
     @Column(name = "CREATED_DATE", updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
     private LocalDateTime createdDate;
 
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @LastModifiedDate
     @Column(name = "LAST_MODIFIED_DATE")
     private LocalDateTime lastModifiedDate;
