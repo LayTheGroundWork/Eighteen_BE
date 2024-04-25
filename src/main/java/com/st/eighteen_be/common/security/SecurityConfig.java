02136c5c09fe29f85584c35985d1a2607e111791
ehgur062300 <ehgur062300@gmail.com>
2024-04-25 16:40:26 +0900

    fix: authorizeRequests(6.x 지원 안함) -> authorizeHttpRequests 변경

--- src/main/java/com/st/eighteen_be/common/security/SecurityConfig.java
+++ src/main/java/com/st/eighteen_be/common/security/SecurityConfig.java
@@ -1,25 +1,28 @@
 package com.st.eighteen_be.common.security;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.st.eighteen_be.common.exception.ErrorCode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.access.AccessDeniedHandler;
 
-import java.util.stream.Stream;
+import java.io.PrintWriter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
@@ -30,30 +33,44 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .headers((headerConfig) -> headerConfig
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // 1번
+                .cors(AbstractHttpConfigurer::disable) // 2번
+                .headers((headerConfig) -> headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // 3번
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin","/h2-console/**","/v1/api/member/**").permitAll()
                 )
-                .authorizeHttpRequests(request ->
-                        request
-                                .requestMatchers(PathRequest.toH2Console())
-                                .permitAll()
-                                .requestMatchers( // <== 여기
-                                        Stream
-                                                .of(PERMIT_ALL_PATTERNS)
-                                                .map(AntPathRequestMatcher::antMatcher)
-                                                .toArray(AntPathRequestMatcher[]::new)
-                                )
-                                .permitAll()
-                                .requestMatchers(AntPathRequestMatcher.antMatcher("/api/**"))
-                                .authenticated()
-                );
+//                .oauth2Login(oauth2Login ->
+//                        oauth2Login.userInfoEndpoint(userInfoEndpoint ->
+//                                userInfoEndpoint.userService(customOAuth2UserService)
+//                        )
+//                )// OAuth 2.0 로그인 구현
 
-
-        return http.build();
+                .exceptionHandling((exceptionConfig) ->
+                        exceptionConfig.authenticationEntryPoint(unauthorizedEntryPoint).accessDeniedHandler(accessDeniedHandler)
+                ).getOrBuild();// 401 403 관련 예외처리
     }
+
+    private final AuthenticationEntryPoint unauthorizedEntryPoint =
+            (request, response, authException) -> {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                String json = new ObjectMapper().writeValueAsString(ErrorCode.UNAUTHORIZED);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                PrintWriter writer = response.getWriter();
+                writer.write(json);
+                writer.flush();
+            };
+
+    private final AccessDeniedHandler accessDeniedHandler =
+            (request, response, accessDeniedException) -> {
+                response.setStatus(HttpStatus.FORBIDDEN.value());
+                String json = new ObjectMapper().writeValueAsString(ErrorCode.FORBIDDEN);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                PrintWriter writer = response.getWriter();
+                writer.write(json);
+                writer.flush();
+            };
 
 }
