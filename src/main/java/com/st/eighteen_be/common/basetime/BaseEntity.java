f3ad54bfc23e8ceb4e5135915173d343d12d0cfb
ehgur062300 <ehgur062300@gmail.com>
2024-04-25 16:31:32 +0900

    fix: 기존 설정 변경

--- src/main/java/com/st/eighteen_be/common/basetime/BaseEntity.java
+++ src/main/java/com/st/eighteen_be/common/basetime/BaseEntity.java
@@ -3,25 +3,32 @@ package com.st.eighteen_be.common.basetime;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-public class BaseEntity {
+public abstract class BaseEntity {
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void onPrePersist() {
+        String customLocalDateTimeFormat = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        LocalDateTime parsedCreateAt = LocalDateTime.parse(customLocalDateTimeFormat, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        this.createdAt = parsedCreateAt;
+        this.lastModifiedDate = parsedCreateAt;
+    }
 }
