39bf9c7c9c601a7da621aa552655e55ea68ecdb0
ehgur062300 <ehgur062300@gmail.com>
2024-04-29 22:55:24 +0900

    feat: 메세지 발송 api 구현

--- src/main/java/com/st/eighteen_be/member/api/MemberApiController.java
+++ src/main/java/com/st/eighteen_be/member/api/MemberApiController.java
@@ -1,17 +1,23 @@
 package com.st.eighteen_be.member.api;
 
 import com.st.eighteen_be.common.response.ApiResponse;
+import com.st.eighteen_be.member.domain.MemberPrivacy;
 import com.st.eighteen_be.member.domain.dto.signIn.SignInRequestDto;
-import com.st.eighteen_be.member.domain.dto.signIn.SignInResponseDto;
 import com.st.eighteen_be.member.service.MemberService;
+import com.st.eighteen_be.sms.domain.SmsCertificationRequestDto;
+import com.st.eighteen_be.sms.service.SmsUtil;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
- * packageName    : com.st.eighteen_be.member.web
+ * packageName    : com.st.eighteen_be.member.api
  * fileName       : MemberApiController
  * author         : ehgur
  * date           : 2024-04-18
@@ -22,7 +28,6 @@ import org.springframework.web.bind.annotation.*;
  * 2024-04-18        ehgur             최초 생성
  */
 
-
 @RestController
 @Slf4j
 @RequiredArgsConstructor
@@ -30,12 +35,19 @@ import org.springframework.web.bind.annotation.*;
 public class MemberApiController {
 
     private final MemberService memberService;
+    private final SmsUtil smsUtil;
 
-    @PostMapping("/save")
-    public ApiResponse<SignInResponseDto> save(@Valid @RequestBody SignInRequestDto requestDto){
+    @PostMapping("/signIn")
+    public ApiResponse<MemberPrivacy> signIn(@Valid @RequestBody SignInRequestDto requestDto){
 
-        memberService.signIn(requestDto);
-        return ApiResponse.success(HttpStatus.OK, );
+        return ApiResponse.success(HttpStatus.OK, memberService.save(requestDto));
     }
 
+    @PostMapping("/sms-certification/sends")
+    public ApiResponse<String> sendsCertification(String phoneNumber){
+
+        smsUtil.sendOne(phoneNumber, smsUtil.createRandomNumber());
+
+        return ApiResponse.success(HttpStatus.OK, phoneNumber + ": 전송 완료");
+    }
 }
