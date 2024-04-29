0243a0f149b4e377459bd26be7250f1e71a7dfa3
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-04-30 00:36:32 +0900

    fix: 카프카 프로듀서 멱등성 설정 서포트를 위한 재시도 설정 MAX_VALUE 로 변경

--- src/main/java/com/st/eighteen_be/config/kafka/KafkaProducerConfig.java
+++ src/main/java/com/st/eighteen_be/config/kafka/KafkaProducerConfig.java
@@ -43,7 +43,7 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        configProps.put(ProducerConfig.RETRIES_CONFIG, 2);
+        configProps.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE);
         configProps.put(ProducerConfig.ACKS_CONFIG, "all");
         configProps.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "zstd");
         configProps.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
