ad900190b436fbf530055d77268a3af9b4402f0c
ehgur062300 <ehgur062300@gmail.com>
2024-04-29 22:51:06 +0900

    feat: 회원 인증을 위한 Entity & DTO

--- src/main/java/com/st/eighteen_be/member/domain/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.st.eighteen_be.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member,Integer> {
-
-}
