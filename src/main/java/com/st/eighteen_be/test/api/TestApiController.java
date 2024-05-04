2a3338c8463516a6808d61a7241031b25c231732
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-05-04 13:27:57 +0900

    fix: 포트 변경

--- src/main/java/com/st/eighteen_be/test/api/TestApiController.java
+++ src/main/java/com/st/eighteen_be/test/api/TestApiController.java
@@ -1,6 +1,7 @@
 package com.st.eighteen_be.test.api;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestApiController {
     
+    @GetMapping("/test")
     public ResponseEntity<Object> test() {
         return ResponseEntity.ok("test");
     }
