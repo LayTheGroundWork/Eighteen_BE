0cf78f6360714a65483e5bca7ae35954c993a8a7
박상준 <qkrtkdwns3410@gmail.com>
2024-05-10 21:33:04 +0900

    feat: 레디스 채팅메시지 카운트 구현중

--- src/main/java/com/st/eighteen_be/chat/service/redis/RedisMessageService.java
+++ src/main/java/com/st/eighteen_be/chat/service/redis/RedisMessageService.java
@@ -1,7 +1,9 @@
 package com.st.eighteen_be.chat.service.redis;
 
+import jakarta.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,5 +21,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Slf4j
 public class RedisMessageService {
-    private final springredis
+    private static final String UNREAD_COUNT_REDIS_KEY = "unreadMessageCount:";
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void incrementUnreadMessageCount(@Nonnull Long receiverNo) {
+        log.info("========== incrementUnreadMessageCount ========== receiverNo : {}", receiverNo);
+
+        redisTemplate.opsForValue().increment(UNREAD_COUNT_REDIS_KEY + receiverNo);
+    }
+
+    public void resetUnreadMessageCount(@Nonnull Long receiverNo) {
+        log.info("========== resetUnreadMessageCount ========== receiverNo : {}", receiverNo);
+
+        redisTemplate.delete(UNREAD_COUNT_REDIS_KEY + receiverNo);
+    }
+
+    public int getUnreadMessageCount(String chatroomId, Long userNo) {
+        log.info("========== getUnreadMessageCount ========== chatroomId : {}, userNo : {}", chatroomId, userNo);
+
+        return (int) redisTemplate.opsForValue().get(UNREAD_COUNT_REDIS_KEY + userNo);
+    }
 }
