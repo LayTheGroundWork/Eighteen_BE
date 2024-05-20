27ea56443f403189da23ff189665f9e30df23a4b
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-05-20 23:01:41 +0900

    chore: 불필요한 클래스 제거

--- src/main/java/com/st/eighteen_be/tournament/repository/GameEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.st.eighteen_be.tournament.repository;
-
-import com.st.eighteen_be.tournament.domain.entity.GameEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameEntityRepository extends JpaRepository<GameEntity, Long> {
-}
