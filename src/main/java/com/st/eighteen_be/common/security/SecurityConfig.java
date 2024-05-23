03bb56a731e9ef8b6a2c651c1014e778def2029f
ehgur062300 <ehgur062300@gmail.com>
2024-05-23 17:59:52 +0900

    fix: AesBytesEncryptor 사용을 위한 Bean 등록 & AuthenticationManager Bean 등록

--- src/main/java/com/st/eighteen_be/common/security/SecurityConfig.java
+++ src/main/java/com/st/eighteen_be/common/security/SecurityConfig.java
@@ -2,79 +2,111 @@ package com.st.eighteen_be.common.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.st.eighteen_be.common.exception.ErrorCode;
-import com.st.eighteen_be.jwt.JwtTokenAuthenticationFilter;
+import com.st.eighteen_be.jwt.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 import java.io.PrintWriter;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtTokenAuthenticationFilter jwtTokenAuthenticationFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CorsFilter corsFilter;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    //권한 확인을 하지 않는 uri
+    // 대칭키
+    @Value("${symmetric.key}")
+    private String symmetricKey;
+
+    @Value("${symmetric.salt}")
+    private String salt;
+
+    // 권한 확인을 하지 않는 URI
     private static final String[] PERMIT_ALL_PATTERNS = new String[] {
-
             "/v1/api/**",
             "/h2-console/**",
-//
-//            "/v1/api/member/**",
-//
-//            "/v1/api/message/**",
     };
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
-
-        return httpSecurity
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        log.info("Security configuration initialized");
+        httpSecurity
                 // REST API이므로 basic auth 및 csrf 보안을 사용하지 않음
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .csrf(AbstractHttpConfigurer::disable) // 1번
-                .cors(AbstractHttpConfigurer::disable) // 2번
-                .headers((headerConfig) -> headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // 3번
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .headers(headerConfig -> headerConfig.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
 
                 // JWT를 사용하기 때문에 세션을 사용하지 않음
                 .sessionManagement(configurer -> configurer
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(PERMIT_ALL_PATTERNS).permitAll()
-                        .requestMatchers("/test").hasAuthority("USER")
                         .anyRequest().authenticated()
                 )
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
-                .addFilterBefore(jwtTokenAuthenticationFilter,
-                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtTokenAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(exceptionConfig ->
+                        exceptionConfig.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .exceptionHandling(e ->
+                        e.authenticationEntryPoint(unauthorizedEntryPoint).accessDeniedHandler(accessDeniedHandler)
+                );
 
-//                .oauth2Login(oauth2Login ->
-//                        oauth2Login.userInfoEndpoint(userInfoEndpoint ->
-//                                userInfoEndpoint.userService(customOAuth2UserService)
-//                        )
-//                )// OAuth 2.0 로그인 구현
-
-                .exceptionHandling((exceptionConfig) ->
-                        exceptionConfig.authenticationEntryPoint(unauthorizedEntryPoint).accessDeniedHandler(accessDeniedHandler)
-                ).getOrBuild();// 401 403 관련 예외처리
+        return httpSecurity.build();
     }
 
+    // AesBytesEncryptor 사용을 위한 Bean 등록
     @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
+    public AesBytesEncryptor aesBytesEncryptor() {
+        return new AesBytesEncryptor(symmetricKey, salt);
+    }
+
+    // PasswordEncoder Bean 등록
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // AuthenticationManager Bean 등록
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity,
+                                                       PasswordEncoder passwordEncoder,
+                                                       UserDetailsService userDetailsService) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                httpSecurity.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+        return authenticationManagerBuilder.build();
     }
 
     private final AuthenticationEntryPoint unauthorizedEntryPoint =
@@ -96,5 +128,5 @@ public class SecurityConfig {
                 writer.write(json);
                 writer.flush();
             };
-
 }
+
