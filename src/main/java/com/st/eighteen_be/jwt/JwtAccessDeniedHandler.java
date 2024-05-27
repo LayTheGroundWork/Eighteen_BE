96cd507fecd859f834126fb19bd31e8561221f86
ehgur062300 <ehgur062300@gmail.com>
2024-05-27 15:10:58 +0900

    fix: Jwt 에러 핸들링 수정

--- src/main/java/com/st/eighteen_be/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/st/eighteen_be/jwt/JwtAccessDeniedHandler.java
@@ -1,20 +1,19 @@
 package com.st.eighteen_be.jwt;
 
-import jakarta.servlet.ServletException;
+import com.st.eighteen_be.common.exception.ErrorCode;
+import com.st.eighteen_be.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) {
         // 필요한 권한이 없이 접근하려 할때 403
-        response.sendError(HttpServletResponse.SC_FORBIDDEN, "권한이 없습니다.");
+        ApiResponse.fail(ErrorCode.TOKEN_FORBIDDEN);
     }
 }
--- src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationEntryPoint.java
@@ -1,19 +1,19 @@
 package com.st.eighteen_be.jwt;
 
+import com.st.eighteen_be.common.exception.ErrorCode;
+import com.st.eighteen_be.common.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "자격 증명 실패");
+        ApiResponse.fail(ErrorCode.TOKEN_UNAUTHORIZED);
     }
 }
