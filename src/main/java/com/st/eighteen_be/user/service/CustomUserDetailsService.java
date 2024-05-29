b5947968cd4074128ec67f66d0d86a7bf9beb853
ehgur062300 <ehgur062300@gmail.com>
2024-05-29 16:04:43 +0900

    fix: CustomUserDetails 수정

--- src/main/java/com/st/eighteen_be/user/service/CustomUserDetailsService.java
+++ src/main/java/com/st/eighteen_be/user/service/CustomUserDetailsService.java
@@ -2,21 +2,16 @@ package com.st.eighteen_be.user.service;
 
 import com.st.eighteen_be.common.exception.ErrorCode;
 import com.st.eighteen_be.common.exception.sub_exceptions.data_exceptions.NotFoundException;
+import com.st.eighteen_be.user.domain.CustomUserDetails;
 import com.st.eighteen_be.user.domain.UserPrivacy;
 import com.st.eighteen_be.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -32,21 +27,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String phoneNumber) throws UsernameNotFoundException {
         return userRepository.findByPhoneNumber(encryptService.encryptPhoneNumber(phoneNumber))
                 .map(this::createUserDetails)
-                .orElseThrow(() ->
-                        new NotFoundException(ErrorCode.SIGN_IN_NOT_FOUND_USER));
+                .orElseThrow(() -> new NotFoundException(ErrorCode.SIGN_IN_NOT_FOUND_USER));
     }
 
     // 해당하는 User 의 데이터가 존재한다면 UserDetails 객체로 만들어서 return
     private UserDetails createUserDetails(UserPrivacy user) {
-
-        List<GrantedAuthority> authorities = user.getAuthorities().stream()
-                .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
-                .collect(Collectors.toList());
-
-        return User.builder()
-                .username(user.getUsername())
-                .password(passwordEncoder.encode(user.getPassword())) // 암호화된 비밀번호를 그대로 사용
-                .authorities(authorities)
-                .build();
+        return CustomUserDetails.of(user, passwordEncoder.encode(""));
     }
 }
