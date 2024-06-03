26b5cb6106d24cd0806e880aed1be918c7670775
ehgur062300 <ehgur062300@gmail.com>
2024-06-03 20:43:27 +0900

    fix: ApiResponse -> ApiResp 파일로 변경

--- src/main/java/com/st/eighteen_be/jwt/JwtAccessDeniedHandler.java
+++ src/main/java/com/st/eighteen_be/jwt/JwtAccessDeniedHandler.java
@@ -1,7 +1,7 @@
 package com.st.eighteen_be.jwt;
 
 import com.st.eighteen_be.common.exception.ErrorCode;
-import com.st.eighteen_be.common.response.ApiResponse;
+import com.st.eighteen_be.common.response.ApiResp;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.AccessDeniedException;
@@ -14,6 +14,6 @@ public class JwtAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) {
         // 필요한 권한이 없이 접근하려 할때 403
-        ApiResponse.fail(ErrorCode.TOKEN_FORBIDDEN);
+        ApiResp.fail(ErrorCode.TOKEN_FORBIDDEN);
     }
 }
--- src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,7 @@
 package com.st.eighteen_be.jwt;
 
 import com.st.eighteen_be.common.exception.ErrorCode;
-import com.st.eighteen_be.common.response.ApiResponse;
+import com.st.eighteen_be.common.response.ApiResp;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -14,6 +14,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
         // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        ApiResponse.fail(ErrorCode.TOKEN_UNAUTHORIZED);
+        ApiResp.fail(ErrorCode.TOKEN_UNAUTHORIZED);
     }
 }
