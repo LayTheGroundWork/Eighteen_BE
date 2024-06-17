b9281107a08cbe530148021b7bb67824e4ee45d7
ehgur <ehgur062300@gmail.com>
2024-06-17 15:51:26 +0900

    fix: 스웨거 추가

--- src/main/java/com/st/eighteen_be/school/dto/SchoolsResponseDto.java
+++ src/main/java/com/st/eighteen_be/school/dto/SchoolsResponseDto.java
@@ -2,6 +2,7 @@ package com.st.eighteen_be.school.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,11 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SchoolsResponseDto {
     @JsonProperty("SCHUL_NM")
+    @Schema(description = "학교 이름", example = "에이틴고등학교")
     private String schoolName;
 
     @JsonProperty("ORG_RDNMA")
+    @Schema(description = "학교 도로명 주소", example = "서울")
     private String roadAddress;
 
     @Builder
