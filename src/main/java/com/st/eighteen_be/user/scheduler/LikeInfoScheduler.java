d7e3d449a090d6466bab31aa00e4e2de41ab48fa
ehgur <ehgur062300@gmail.com>
2024-07-19 08:56:01 +0900

    refactor: 메서드 통합

--- src/main/java/com/st/eighteen_be/user/scheduler/LikeInfoScheduler.java
+++ src/main/java/com/st/eighteen_be/user/scheduler/LikeInfoScheduler.java
@@ -1,10 +1,8 @@
 package com.st.eighteen_be.user.scheduler;
 
 import com.st.eighteen_be.user.service.LikeService;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -25,24 +23,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LikeInfoScheduler {
 
-    private final RedisTemplate<String,String> redisLikeTemplate;
     private final LikeService likeService;
 
     @Scheduled(cron = "0 0 2 * * ?")
-    @Transactional
-    protected void backupUserLikeDataToMySQL(){
-
+    public void backupDataToMySQL(){
         likeService.backupUserLikeDataToMySQL();
-
         log.info("user likes update complete");
-    }
-
-    @Scheduled(cron = "0 0 2 * * ?")
-    @Transactional
-    protected void backupLikeCountToMySQL(){
 
         likeService.backupLikeCountToMySQL();
-
         log.info("user like count update complete");
     }
 }
