384676edac9a69cb9e186bfe1eef5978417a0eed
ehgur <ehgur062300@gmail.com>
2024-07-24 01:33:55 +0900

    refactor: 유효성 검사 추가

--- src/main/java/com/st/eighteen_be/CreateTester.java
+++ src/main/java/com/st/eighteen_be/CreateTester.java
@@ -9,37 +9,46 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class CreateTester {
 
+    private static final String TESTER_PHONE_NUMBER = "01012345678";
+    private static final String TESTER_NICKNAME = "tester1";
+    private static final LocalDate TESTER_BIRTHDAY = LocalDate.of(1999, 12, 23);
+    private static final SchoolData TESTER_SCHOOL_DATA = new SchoolData("테스터고등학교", "서울");
+    private static final String TESTER_UNIQUE_ID = "@Tester";
+    private static final List<String> TESTER_ROLES = List.of("USER");
+
     private final UserRepository userRepository;
 
     @PostConstruct
-    public void createTester(){
-        String phoneNumber = "01012345678";
-        String nickName = "tester1";
-        LocalDate birthDay = LocalDate.of(1999,12,23);
-        SchoolData schoolData = new SchoolData("테스터고등학교", "서울");
-        String uniqueId = "@Tester";
-        List<String> roles = new ArrayList<>();
-        roles.add("USER");
+    public void createTester() {
+        Optional<UserInfo> existingTester = userRepository.findByUniqueId(TESTER_UNIQUE_ID);
+
+        if (existingTester.isPresent()) {
+            log.info("Tester already exists with unique ID: {}", TESTER_UNIQUE_ID);
+            return;
+        }
 
         UserInfo tester = UserInfo.builder()
-                .phoneNumber(phoneNumber)
-                .nickName(nickName)
-                .birthDay(birthDay)
-                .schoolData(schoolData)
-                .uniqueId(uniqueId)
-                .roles(roles)
+                .phoneNumber(TESTER_PHONE_NUMBER)
+                .nickName(TESTER_NICKNAME)
+                .birthDay(TESTER_BIRTHDAY)
+                .schoolData(TESTER_SCHOOL_DATA)
+                .uniqueId(TESTER_UNIQUE_ID)
+                .roles(TESTER_ROLES)
                 .build();
 
-        userRepository.save(tester);
-
+        try {
+            userRepository.save(tester);
+            log.info("Tester created successfully: {}", tester);
+        } catch (Exception e) {
+            log.error("Error creating tester: ", e);
+        }
     }
 }
