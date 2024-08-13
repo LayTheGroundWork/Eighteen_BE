e5b223f93951d8c5a815de130a9923f15999f044
ehgur <ehgur062300@gmail.com>
2024-08-13 09:02:44 +0900

    refactor: SpringBoot 3.2 - 매개변수 이름 인식 문제 해결을 위해 어노테이션에 이름 생략안함

--- src/main/java/com/st/eighteen_be/school/controller/SchoolApiController.java
+++ src/main/java/com/st/eighteen_be/school/controller/SchoolApiController.java
@@ -26,7 +26,7 @@ public class SchoolApiController {
 
     @Operation(summary = "학교검색", description = "학교검색")
     @GetMapping("/v1/api/schools")
-    public ApiResp<List<SchoolsResponseDto>> searchSchools(@RequestParam String schoolName) {
+    public ApiResp<List<SchoolsResponseDto>> searchSchools(@RequestParam("schoolName") String schoolName) {
         return ApiResp.success(HttpStatus.OK, schoolService.searchSchools(schoolName).block());
     }
 }
