6fc0ddcb2e0829f239dee78c53cfe90575320467
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-08-29 00:05:10 +0900

    test: 랜덤 16 고르기 스케쥴러 테스트 코드 추가

--- src/test/java/com/st/eighteen_be/tournament/scheduler/TournamentBatchTest.java
+++ src/test/java/com/st/eighteen_be/tournament/scheduler/TournamentBatchTest.java
@@ -1,15 +1,25 @@
 package com.st.eighteen_be.tournament.scheduler;
 
+import com.st.eighteen_be.common.annotation.ServiceWithRedisTest;
+import com.st.eighteen_be.common.extension.RedisTestContainerExtenstion;
+import com.st.eighteen_be.tournament.domain.redishash.RandomUser;
 import com.st.eighteen_be.tournament.service.TournamentService;
+import com.st.eighteen_be.user.dto.response.UserRandomResponseDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 /**
  * packageName    : com.st.eighteen_be.tournament.batch
@@ -23,31 +33,63 @@ import static org.mockito.Mockito.verify;
  * 24. 5. 19.        ipeac       최초 생성
  */
 @DisplayName("토너먼트 배치 테스트")
+@ServiceWithRedisTest
 @ExtendWith(MockitoExtension.class)
-public class TournamentBatchTest {
-    @InjectMocks
+public class TournamentBatchTest extends RedisTestContainerExtenstion {
     private TournamentScheduler tournamentScheduler;
-    
-    @Mock
+
+    @Autowired
+    private RedisTemplate<String, RandomUser> redisTemplate;
+
+    @MockBean
     private TournamentService tournamentService;
-    
+
+    @BeforeEach
+    void setUp() {
+        tournamentScheduler = new TournamentScheduler(tournamentService, redisTemplate);
+        redisTemplate.delete("pickedRandomUser");
+    }
+
     @Test
     @DisplayName("startNewTournaments 스케쥴러 정상 동작 테스트 - enums 길이만큼 startTournament 호출")
     void When_startNewTournaments_enumLength_Then_startTournament() {
         //when
         tournamentScheduler.startNewTournaments();
-        
+
         //then
         verify(tournamentService, times(1)).startTournament();
     }
-    
+
     @Test
     @DisplayName("endTournaments 스케쥴러 정상 동작 테스트")
     void When_endTournaments_Then_endLastestTournaments() {
         //when
         tournamentScheduler.endTournaments();
-        
+
         //then
         verify(tournamentService, times(1)).endLastestTournaments();
+    }
+
+    @Test
+    @DisplayName("pickRandomUser 스케쥴러 정상 동작 테스트")
+    void When_pickRandomUser_Then_pickRandomUser() {
+        // given
+        List<UserRandomResponseDto> randomUsers = Collections.singletonList(
+                UserRandomResponseDto.of(1, "https://example.com/profile.jpg")
+        );
+        when(tournamentService.pickRandomUser()).thenReturn(randomUsers);
+
+        // when
+        tournamentScheduler.pickRandomUser();
+
+        // then
+        HashOperations<String, String, RandomUser> hashOperations = redisTemplate.opsForHash();
+        RandomUser storedUser = hashOperations.get("pickedRandomUser", "1");
+
+        assertThat(storedUser).isNotNull();
+        assertThat(storedUser.getUserId()).isEqualTo("1");
+        assertThat(storedUser.getProfileImageUrl()).isEqualTo("https://example.com/profile.jpg");
+
+        verify(tournamentService, times(1)).pickRandomUser();
     }
 }
