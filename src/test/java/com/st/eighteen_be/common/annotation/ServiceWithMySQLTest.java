0499274167d48363d54ed3548f04e7ff5bf78c72
박상준 <qkrtkdwns3410@gmail.com>
2024-09-01 16:46:15 +0900

    test: 로직 수정에 따른 테스트 코드 수정

--- src/test/java/com/st/eighteen_be/common/annotation/ServiceWithMySQLTest.java
+++ src/test/java/com/st/eighteen_be/common/annotation/ServiceWithMySQLTest.java
@@ -3,6 +3,7 @@ package com.st.eighteen_be.common.annotation;
 import com.st.eighteen_be.config.advice.audit.JpaAuditingConfig;
 import com.st.eighteen_be.config.querydsl.QuerydslConfig;
 import com.st.eighteen_be.config.repository.JpaConfig;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -16,7 +17,7 @@ import java.lang.annotation.Target;
 // 테스트용 어노테이션 생성
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY ,connection = EmbeddedDatabaseConnection.H2)
 @Import({JpaAuditingConfig.class, JpaConfig.class, QuerydslConfig.class})
 @DataJpaTest
 @ActiveProfiles("test")
