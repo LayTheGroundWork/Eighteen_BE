89a035aee24b6f3cc46027335baffc7117fb367c
ehgur062300 <ehgur062300@gmail.com>
2024-09-04 00:25:54 +0900

    TODO: 마이페이지 컨트롤러와 서비스 추가해야함

--- src/main/java/com/st/eighteen_be/user/api/MyPageApiController.java
+++ src/main/java/com/st/eighteen_be/user/api/MyPageApiController.java
@@ -1,10 +1,15 @@
 package com.st.eighteen_be.user.api;
 
+import com.st.eighteen_be.common.response.ApiResp;
+import com.st.eighteen_be.user.dto.request.MyPageRequestDto;
 import com.st.eighteen_be.user.service.MyPageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,9 @@ public class MyPageApiController {
 
     private final MyPageService myPageService;
 
-    //@Operation(summary = "")
+    @Operation(summary = "myPage 저장", description = "myPage 저장")
+    @PostMapping("/v1/api/mypage/save")
+    public ApiResp<MyPageRequestDto> saveMyPage(@RequestBody MyPageRequestDto request) {
+        return ApiResp.success(HttpStatus.OK, myPageService.save(request));
+    }
 }
--- src/main/java/com/st/eighteen_be/user/service/MyPageService.java
+++ src/main/java/com/st/eighteen_be/user/service/MyPageService.java
@@ -14,5 +14,10 @@ public class MyPageService {
 
     private final UserRepository userRepository;
 
-    
+    //TODO: mbti, 자기소개글, 질문리스트 작성(최소 3개) 추가 및 수정 기능 만들기
+    // 마이 페이지는 첫 단계에서는 프론트에서 넘어온 값만 저장하면 됨
+    // 이미 값이 존재하고 수정 api를 호출할 회원 id와 어떤 항목을 수정할건지 넘겨주면 됨
+
+
+
 }
