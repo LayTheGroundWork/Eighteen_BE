e6d8f88a408e1e2e46203f73077bad41c14a7ae5
ehgur <ehgur062300@gmail.com>
2024-10-14 22:29:07 +0900

    refactor: jwt 에외처리 Filter에서 처리하기

--- src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationFilter.java
@@ -1,11 +1,20 @@
 package com.st.eighteen_be.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.st.eighteen_be.common.exception.ErrorCode;
+import com.st.eighteen_be.common.exception.sub_exceptions.data_exceptions.BadRequestException;
+import com.st.eighteen_be.common.exception.sub_exceptions.data_exceptions.NotValidException;
+import com.st.eighteen_be.common.response.ApiResp;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -19,6 +28,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     // 실제 필터링 로직은 doFilterInternal 에 들어감
     // JWT 토큰의 인증 정보를 현재 쓰레드의 SecurityContext 에 저장하는 역할 수행
@@ -31,14 +41,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String accessToken = request.getHeader("Authorization");
         String jwt = jwtTokenProvider.resolveAccessToken(accessToken);
 
+
         // 2. validateToken 으로 토큰 유효성 검사
         // 정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
-        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
-            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+        try {
+            if (jwtTokenProvider.validateToken(jwt)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+
+        } catch (BadRequestException e){
+            response.getWriter().print(objectMapper.writeValueAsString(
+                    ApiResp.fail(ErrorCode.EXPIRED_TOKEN)
+            ));
+            return;
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void setResponse(HttpServletResponse response) throws IOException {
+        var responseEntity = ResponseEntity.status(ErrorCode.EXPIRED_TOKEN.getStatus()).build();
+        response.getWriter().print(responseEntity);
     }
 
 }
