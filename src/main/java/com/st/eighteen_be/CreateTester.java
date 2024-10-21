62d4c44369abf93fbbe952eb9d334cfe5fd90549
ehgur <ehgur062300@gmail.com>
2024-10-21 23:40:43 +0900

    refactor(CreateTester): Tester 32명 생성

--- src/main/java/com/st/eighteen_be/CreateTester.java
+++ src/main/java/com/st/eighteen_be/CreateTester.java
@@ -7,10 +7,11 @@ import com.st.eighteen_be.user.domain.UserRoles;
 import com.st.eighteen_be.user.enums.CategoryType;
 import com.st.eighteen_be.user.enums.RolesType;
 import com.st.eighteen_be.user.repository.UserRepository;
+import com.st.eighteen_be.user.service.EncryptService;
 import jakarta.annotation.PostConstruct;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -20,91 +21,77 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CreateTester {
 
-    private static final List<String> TESTER_PHONE_NUMBER = List.of("01012345678","01012341234");
-    private static final List<String> TESTER_ENCRYPT_PHONE_NUMBER = List.of(
-            "49 87 37 -20 56 2 107 68 54 120 -37 -100 -115 119 -61 -57 ",
-            "59 7 73 93 23 30 -3 82 -33 -114 -65 12 -62 -56 66 13 "
-    );
-    private static final List<String> TESTER_NICKNAME = List.of("kim","lee");
-    private static final List<LocalDate> TESTER_BIRTHDAY = List.of(
-            LocalDate.of(2010, 12, 23),
-            LocalDate.of(2013, 12, 6)
-    );
-    private static final List<SchoolData> TESTER_SCHOOL_DATA = List.of(
-            new SchoolData("테스터고등학교", "서울"),
-            new SchoolData("테스터중학교", "정선")
-    );
-    private static final List<String> TESTER_UNIQUE_ID = List.of("tester1","tester2");
-    private static final String IMAGE_KEY = "testKey";
-    private static final String THUMBNAIL_IMAGE_KEY = "thumbnail_testKey";
-    private static final boolean IS_THUMBNAIL = true;
+    private static final int TESTER_COUNT = 32;
+    private static final String BASE_PHONE_NUMBER = "01000000000";
+    private static final String BASE_NICKNAME = "user_";
+    private static final String BASE_UNIQUE_ID = "tester";
 
-    private static final boolean TOURNAMENT_JOIN = true;
-
+    private static final List<String> SCHOOLS = Arrays.asList(
+            "서울고등학교", "부산고등학교", "대구고등학교",
+            "인천고등학교", "광주고등학교", "대전고등학교",
+            "울산고등학교", "경기도고등학교", "강원도고등학교"
+    );
 
     private final UserRepository userRepository;
+    private final EncryptService encryptService;
 
     @PostConstruct
-    public void createTester() {
-        Optional<UserInfo> user1 = userRepository.findByUniqueId(TESTER_UNIQUE_ID.get(0));
-        Optional<UserInfo> user2 = userRepository.findByUniqueId(TESTER_UNIQUE_ID.get(1));
+    public void createTesters() {
+        // 기존 데이터 삭제
+        userRepository.deleteAll();
 
-        if(user1.isPresent() || user2.isPresent()){
-            userRepository.deleteAll();
+        for (int i = 0; i < TESTER_COUNT; i++) {
+            String phoneNumber = generatePhoneNumber(i+1);
+            String nickName = BASE_NICKNAME + (i + 1);
+            LocalDate birthDay = generateRandomBirthday();
+            String uniqueId = BASE_UNIQUE_ID + (i + 1);
+            SchoolData schoolData = new SchoolData(randomSchool(i%SCHOOLS.size()), "지역");
+
+            String encryptNumber = encryptService.encryptPhoneNumber(phoneNumber);
+
+            UserInfo tester = UserInfo.builder()
+                    .thumbnail("thumbnail_testKey")
+                    .phoneNumber(encryptNumber)
+                    .nickName(nickName)
+                    .birthDay(birthDay)
+                    .schoolData(schoolData)
+                    .uniqueId(uniqueId)
+                    .category(CategoryType.ETC)
+                    .tournamentJoin(true)
+                    .build();
+
+            UserRoles userRoles = UserRoles.builder()
+                    .role(RolesType.USER)
+                    .build();
+            userRoles.setUser(tester);
+
+            UserMediaData userMediaData = UserMediaData.builder()
+                    .imageKey("testKey")
+                    .isThumbnail(true)
+                    .build();
+            userMediaData.setUser(tester);
+
+            try {
+                userRepository.save(tester);
+                log.info("Tester created successfully: phone:{} / uniqueId:{}", phoneNumber,tester.getUniqueId());
+            } catch (Exception e) {
+                log.error("Error creating tester: ", e);
+            }
         }
+    }
 
-        UserInfo tester1 = UserInfo.builder()
-                .thumbnail(THUMBNAIL_IMAGE_KEY)
-                .phoneNumber(TESTER_ENCRYPT_PHONE_NUMBER.get(0))
-                .nickName(TESTER_NICKNAME.get(0))
-                .birthDay(TESTER_BIRTHDAY.get(0))
-                .schoolData(TESTER_SCHOOL_DATA.get(0))
-                .uniqueId(TESTER_UNIQUE_ID.get(0))
-                .category(CategoryType.ETC)
-                .tournamentJoin(TOURNAMENT_JOIN)
-                .build();
+    private String generatePhoneNumber(int index) {
+        return BASE_PHONE_NUMBER.substring(0, BASE_PHONE_NUMBER.length() - 2) + String.format("%02d", index);
+    }
 
-        UserInfo tester2 = UserInfo.builder()
-                .thumbnail(THUMBNAIL_IMAGE_KEY)
-                .phoneNumber(TESTER_ENCRYPT_PHONE_NUMBER.get(1))
-                .nickName(TESTER_NICKNAME.get(1))
-                .birthDay(TESTER_BIRTHDAY.get(1))
-                .schoolData(TESTER_SCHOOL_DATA.get(1))
-                .uniqueId(TESTER_UNIQUE_ID.get(1))
-                .category(CategoryType.ETC)
-                .tournamentJoin(TOURNAMENT_JOIN)
-                .build();
+    private LocalDate generateRandomBirthday() {
+        int year = 2004 + (int) (Math.random() * 4); // 2004~2007
+        int month = 1 + (int) (Math.random() * 12); // 1~12
+        int day = 1 + (int) (Math.random() * 28); // 1~28
+        return LocalDate.of(year, month, day);
+    }
 
-        UserRoles userRoles1 = UserRoles.builder()
-                .role(RolesType.USER)
-                .build();
-        userRoles1.setUser(tester1);
-
-        UserRoles userRoles2 = UserRoles.builder()
-                .role(RolesType.USER)
-                .build();
-        userRoles2.setUser(tester2);
-
-        UserMediaData userMediaData1 = UserMediaData.builder()
-                .imageKey(IMAGE_KEY)
-                .isThumbnail(IS_THUMBNAIL)
-                .build();
-
-        userMediaData1.setUser(tester1);
-
-        UserMediaData userMediaData2 = UserMediaData.builder()
-                .imageKey(IMAGE_KEY)
-                .isThumbnail(IS_THUMBNAIL)
-                .build();
-        userMediaData2.setUser(tester2);
-
-        try {
-            userRepository.save(tester1);
-            log.info("Tester created successfully: {}", tester1);
-            userRepository.save(tester2);
-            log.info("Tester created successfully: {}", tester2);
-        } catch (Exception e) {
-            log.error("Error creating testers: ", e);
-        }
+    private String randomSchool(int random) {
+        return SCHOOLS.get(random);
     }
 }
