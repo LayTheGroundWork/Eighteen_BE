f9d556ffab56179c1468e359e9a479f626df0f59
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-10-23 23:24:05 +0900

    feat: 토너먼트 유저 보여주는 api

---
+++ src/main/java/com/st/eighteen_be/tournament_participant/api/TournamentParticipantApiController.java
@@ -0,0 +1,22 @@
+package com.st.eighteen_be.tournament_participant.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : com.st.eighteen_be.tournament_participant.api
+ * fileName       : TournamentParticipantApiController
+ * author         : ipeac
+ * date           : 24. 10. 23.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 23.        ipeac       최초 생성
+ */
+@RestController
+@RequestMapping("/v1/api/tournament/participant")
+public class TournamentParticipantApiController {
+
+
+}
