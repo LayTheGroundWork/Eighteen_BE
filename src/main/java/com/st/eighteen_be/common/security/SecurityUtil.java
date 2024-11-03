c121f05b0d4687431074bba069f603463233448c
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-11-03 22:44:03 +0900

    fix: CustomUserDetail -> UserDetail 로 변경

--- src/main/java/com/st/eighteen_be/common/security/SecurityUtil.java
+++ src/main/java/com/st/eighteen_be/common/security/SecurityUtil.java
@@ -2,9 +2,9 @@ package com.st.eighteen_be.common.security;
 
 import com.st.eighteen_be.common.exception.ErrorCode;
 import com.st.eighteen_be.common.exception.sub_exceptions.data_exceptions.BadRequestException;
-import com.st.eighteen_be.user.domain.CustomUserDetails;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class SecurityUtil {
     public static String getCurrentUsername() {
@@ -15,7 +15,7 @@ public class SecurityUtil {
         return authentication.getName();
     }
     
-    public static void checkUser(CustomUserDetails userDetails) {
+    public static void checkUser(UserDetails userDetails) {
         if (userDetails == null) {
             throw new BadRequestException(ErrorCode.UNAUTHORIZED_TOKEN);
         }
--- src/main/java/com/st/eighteen_be/tournament_participant/api/TournamentParticipantApiController.java
+++ src/main/java/com/st/eighteen_be/tournament_participant/api/TournamentParticipantApiController.java
@@ -4,7 +4,6 @@ import com.st.eighteen_be.common.response.ApiResp;
 import com.st.eighteen_be.common.security.SecurityUtil;
 import com.st.eighteen_be.tournament.domain.redishash.ThisWeekTournamentParticipantResponseDTO;
 import com.st.eighteen_be.tournament_participant.service.TournamentParticipantService;
-import com.st.eighteen_be.user.domain.CustomUserDetails;
 import com.st.eighteen_be.user.enums.CategoryType;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -13,6 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,10 +45,10 @@ public class TournamentParticipantApiController {
             @PathVariable("category") CategoryType category,
             
             @Parameter(hidden = true)
-            @AuthenticationPrincipal CustomUserDetails userDetails
+            @AuthenticationPrincipal UserDetails userDetails
     ) {
         SecurityUtil.checkUser(userDetails);
         
-        return ApiResp.success(HttpStatus.OK, tournamentParticipantService.showParticipantNowTournament(category, userDetails.getUniqueId()));
+        return ApiResp.success(HttpStatus.OK, tournamentParticipantService.showParticipantNowTournament(category, userDetails.getUsername()));
     }
 }
