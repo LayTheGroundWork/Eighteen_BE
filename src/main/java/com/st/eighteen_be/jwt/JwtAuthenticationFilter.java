4700d262b819293aeb38ec5dda690d1907096527
ehgur062300 <ehgur062300@gmail.com>
2024-11-05 01:09:37 +0900

    feat(JwtAuthenticationFilter): 토큰 재발급 api일 경우 filter 통과

--- src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/st/eighteen_be/jwt/JwtAuthenticationFilter.java
@@ -46,8 +46,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 1. Request Header 에서 토큰을 꺼냄ㅕ
         String accessToken = request.getHeader("Authorization");
         String jwt = jwtTokenProvider.resolveAccessToken(accessToken);
-
-        if(StringUtils.hasText(jwt)){
+        
+        if(StringUtils.hasText(jwt) && !request.getRequestURI().contains("/reissued")){
             // 2. validateToken 으로 토큰 유효성 검사
             // 정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
             try {
