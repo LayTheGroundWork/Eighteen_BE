edc346616b4f6568b505f1e7e9d787a5644cb184
ehgur062300 <ehgur062300@gmail.com>
2024-12-23 14:23:02 +0900

    todo: 서비스 수정 필요

--- src/main/java/com/st/eighteen_be/user/service/MyPageService.java
+++ src/main/java/com/st/eighteen_be/user/service/MyPageService.java
@@ -1,7 +1,5 @@
 package com.st.eighteen_be.user.service;
 
-import com.st.eighteen_be.user.domain.UserInfo;
-import com.st.eighteen_be.user.domain.UserMediaData;
 import com.st.eighteen_be.user.dto.request.MyPageRequestDto;
 import com.st.eighteen_be.user.dto.response.UserDetailsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,43 +7,31 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class MyPageService {
 
-    private final UserService userService;
     private final UserDtoService userDtoService;
 
     @Transactional(readOnly = true)
     public UserDetailsResponseDto view(String uniqueId){
-
         return userDtoService.findByUniqueId(uniqueId);
     }
 
     public void update(String uniqueId, MyPageRequestDto requestDto) {
-        UserInfo userInfo = userService.findByUniqueId(uniqueId);
-
-        userInfo.myPageUpdate(requestDto);
+        userDtoService.myPageUpdate(uniqueId, requestDto);
     }
 
-    public void thumbnailUpdate(String uniqueId, UserMediaData userMediaData){
-        UserInfo user = userService.findByUniqueId(uniqueId);
-
-        thumbnailFlagChange(user.getMediaDataList());
-
-        userMediaData.thumbnailFlagUpdate(); // 새로운 썸네일이라고 표시
-        user.thumbnailUpdate(userMediaData);
-
+    // 프로필 이미지 삭제
+    public void profileDelete(String uniqueId, String imageKey) {
+        userDtoService.profileDelete(uniqueId, imageKey);
     }
 
-    private void thumbnailFlagChange(List<UserMediaData> userMediaDataList){
-        for (UserMediaData mediaData : userMediaDataList) {
-            if(mediaData.isThumbnail())
-                mediaData.thumbnailFlagUpdate();
-        }
-    }
+    // 대표 이미지 수정
+
+
+
+
 }
