c3c03dd7923905cb35db723ce53a53de8451df84
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-12-27 00:00:05 +0900

    feat: 돌겠네..

--- src/main/java/com/st/eighteen_be/config/redis/RedisConfig.java
+++ src/main/java/com/st/eighteen_be/config/redis/RedisConfig.java
@@ -1,5 +1,10 @@
 package com.st.eighteen_be.config.redis;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -57,9 +62,9 @@ public class RedisConfig {
         
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setValueSerializer(genericJackson2JsonRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashValueSerializer(genericJackson2JsonRedisSerializer());
         
         return template;
     }
@@ -69,10 +74,22 @@ public class RedisConfig {
         RedisSerializationContext<String, T> serializationContext = RedisSerializationContext
                                                                             .<String, T>newSerializationContext(new StringRedisSerializer())
                                                                             .hashKey(new StringRedisSerializer())
-                                                                            .hashValue(new GenericJackson2JsonRedisSerializer())
+                                                                            .hashValue(genericJackson2JsonRedisSerializer())
                                                                             .build();
         
         return new ReactiveRedisTemplate<>(connectionFactory, serializationContext);
+    }
+    
+    @Bean
+    public GenericJackson2JsonRedisSerializer genericJackson2JsonRedisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        
+        objectMapper.setVisibility(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.deactivateDefaultTyping(); // _class 필드를 무시하도록 설정
+        
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
     }
 
 //    @PostConstruct
